b10f5e668a183bb071c8e674cc36934a4a096a20
vs-rathod30 <49834960+vs-rathod30@users.noreply.github.com>
2019-05-17 17:13:15 +0530

    Add files via upload

--- share_holder_migration_job.rb
+++ share_holder_migration_job.rb
@@ -1,93 +1,109 @@
 module FileMigrationJobs
-    class ShareHolderMigrationJob < FileMigrationJobs::GenericFileMigrationJob
-        Sheet = "Shareholder"
+  class ShareHoldingMigrationJob < FileMigrationJobs::GenericFileMigrationJob
+    Sheet = "Shareholder"
 
-        EXCEL_COLUMN_TO_MODEL_ATTRIBUTE_HASH = { "Shareholding Date" => :shareholding_date, "Shareholder type" => :shareholder_type,
-        "Shareholder Name" => :shareholder_name, "Total no of shares" => :total_no_of_shares, "Total amount invested" => :total_amt_invested,
-        "Total shareholding percent" => :total_shareholding_percent, "Type of shares" => :types_of_shares }
+    EXCEL_COLUMN_TO_MODEL_ATTRIBUTE_HASH = {"Shareholding Date" => :share_holding_date, "Shareholder type" => :share_holder_type,
+                                            "Shareholder Name" => :share_holder_name, "Total no of shares" => :total_number_of_shares, "Total amount invested" => :total_amount_invested,
+                                            "Total shareholding percent" => :total_share_holding_percent, "Type of shares" => :type_of_shares}
 
-        Instrument_type_hash = {"Equity" => :equity, "Ocps" => :ocps, "Ccps" => :ccps, "Ccd" => :ccd, "Ocd" => :ocd, "Warrant" => :warrant}
+    INSTRUMENT_TYPE_HASH = {"Equity" => :equity, "Ocps" => :ocps, "Ccps" => :ccps, "Ccd" => :ccd, "Ocd" => :ocd, "Warrant" => :warrant}
 
-        attr_accessor :attribute_shareholder_index, :customer_id, :share_holder_hash
+    SHARE_HOLDER_TYPE_HASH = {"Promoter" => :promoter, "Promoter Entity" => :promoter_entity, "Angels, Family & Friends" => :angels_family_friends,
+                              "ESOP Pool" => :esop_pool, "Co-founder" => :co_founder, "PE / VC" => :pe_vc, "Other Institution" => :other_institution}
 
-        def initialize
-            self.share_holder_hash = Hash.new{ |k,v| k[v] = [] }
-            self.attribute_shareholder_index = Hash.new
-        end
+    attr_accessor :customer_id, :share_holder_hash
 
-        def parse_document_and_migrate(params)
-            self.customer_id = params[:customer_id]
-            shareholder_file = Roo::Spreadsheet.open(params[:temp_file_name]) 
-            find_index(shareholder_file.sheet(Sheet))
-            iterate_share_holder_sheet(shareholder_file.sheet(Sheet))            
-            
-           # FileUtils.rm params[:temp_file_name], force: true
-        end
+    def initialize
+      self.share_holder_hash = Hash.new { |k, v| k[v] = [] }
+    end
+
+    def parse_document_and_migrate(params)
+      self.customer_id = params[:customer_id]
+      shareholder_file = Roo::Spreadsheet.open(params[:temp_file_name])
+      iterate_share_holders_sheet(shareholder_file.sheet(Sheet))
+      migrate_share_holding_patterns()
+      share_holder_hash.clear
+      # FileUtils.rm params[:temp_file_name], force: true
+    end
 
     private
-        def iterate_share_holder_sheet(shareholder_sheet)
-            it = 1
-            last_row_count = shareholder_sheet.last_row
-            while it < last_row_count
-                 	 it = it + 1
-                    current_row = shareholder_sheet.row(it)
-                    next if current_row.blank? || current_row[0].blank?  
-                                     
-                    self.share_holder_hash["#{current_row[self.attribute_shareholder_index["Shareholding Date"]]}"] << current_row        
-            end
-            migrate_shareholders_hash()
-        end
 
-        def migrate_shareholders_hash()
-            self.share_holder_hash.each do |key, value|
-                shareholding_pattern = ShareHoldingPattern.new
-                shareholding_pattern.date = key
-                shareholding_pattern.customer_id = self.customer_id
-                save_share_holders(shareholding_pattern, key)
-            end
-        end
-
-        def save_share_holders(shareholding_pattern, kkey)
-            i=0
-            while i < self.share_holder_hash[kkey].count do           
-                share_holder_entry = set_share_holder(kkey, i)
-                 i = i + 1
-             shareholding_pattern.share_holders << share_holder_entry
-            end
-            shareholding_pattern.save
-        end
-
-        def set_share_holder(kkey, i)
-            share_holder_entry = ShareHolder.new
-   
-            share_holder_entry.share_holder_name = self.share_holder_hash[kkey][i][self.attribute_shareholder_index["Shareholder Name"]]
-            share_holder_entry.share_holder_type = self.share_holder_hash[kkey][i][self.attribute_shareholder_index["Shareholder type"]]
-            share_holder_entry.total_number_of_shares = self.share_holder_hash[kkey][i][self.attribute_shareholder_index["Total no of shares"]]
-            share_holder_entry.total_amount_invested = self.share_holder_hash[kkey][i][self.attribute_shareholder_index["Total amount invested"]]
-            share_holder_entry.total_share_holding_percent = self.share_holder_hash[kkey][i][self.attribute_shareholder_index["Total shareholding percent"]]
-            if self.attribute_shareholder_index["Type of shares"]
-            tmp = self.share_holder_hash[kkey][i][self.attribute_shareholder_index["Type of shares"]]
-            else tmp = nil
-            end
-            instrument =  set_instrument_details(Instrument_type_hash[tmp])   if tmp
-            share_holder_entry.instrument_details << instrument       if tmp
-            share_holder_entry
-        end
-
-        def set_instrument_details(instrument_type)
-            instrument = InstrumentDetail.new
-            instrument.instrument_type = instrument_type
-            instrument  
-        end
-
-        def find_index(sheet_name)
-            excel_attribute = sheet_name.row(1)
-             excel_attribute.each_with_index do |element, index|
-              if EXCEL_COLUMN_TO_MODEL_ATTRIBUTE_HASH["#{element}"]
-                self.attribute_shareholder_index["#{element}"] = index
-              end
-            end
-        end
-
+    def iterate_share_holders_sheet(shareholders_sheet)
+      it = 1
+      last_row_count = shareholders_sheet.last_row
+      while it < last_row_count
+        current_row = shareholders_sheet.row(it)
+        next if current_row.blank? || current_row[0].blank? || current_row[1].blank?
+            iterate_row_by_row_sheet(shareholders_sheet, it, current_row)
+          return
+        it = it + 1
+      end
+      
     end
+
+    def iterate_row_by_row_sheet(shareholders_sheet, row_index, header_row)
+      it = row_index
+      while it < shareholders_sheet.last_row
+          it = it + 1
+          curr_row = shareholders_sheet.row(it)
+          next if curr_row.blank? || curr_row[0].blank?
+          store_share_holders_into_hash(curr_row, header_row)
+      end
+    end
+
+    def store_share_holders_into_hash(current_row, header_row)
+      date = nil
+      tmp = Hash.new
+      current_row.each_with_index do |element,index|
+          key = EXCEL_COLUMN_TO_MODEL_ATTRIBUTE_HASH[header_row[index]]
+        if key.blank?
+          Rails.logger.info "Invalid Column name #{header_row[index]} mapped."
+          next
+        elsif key == :type_of_shares
+          element = INSTRUMENT_TYPE_HASH[element]
+        elsif key == :share_holder_type
+          element = SHARE_HOLDER_TYPE_HASH[element]
+        elsif key == :share_holding_date
+            date = element
+          next
+        end
+        tmp[key] = element
+      end
+      i = self.share_holder_hash[date].count
+        self.share_holder_hash[date][i] = tmp 
+    end
+
+    def migrate_share_holding_patterns
+   
+      self.share_holder_hash.each do |key, value|
+        shareholdingpattern = set_share_holding_pattern(key)
+
+        value.each do |data|
+             #byebug
+              instrument = set_instrument_details(data) if data[:type_of_shares]
+              data.tap{|k| k.delete(:type_of_shares)} 
+        
+
+          shareholder = ShareHolder.new(data)
+          shareholder.instrument_details << instrument if instrument
+          shareholdingpattern.share_holders << shareholder
+        end
+        shareholdingpattern.save
+      end
+    end
+
+    def set_share_holding_pattern(date)
+        shareholdingpattern = ShareHoldingPattern.new
+        shareholdingpattern.date = date
+        shareholdingpattern.customer_id = self.customer_id
+        shareholdingpattern
+    end
+
+    def set_instrument_details(data)
+        instrument = InstrumentDetail.new
+        instrument.instrument_type = data[:type_of_shares] 
+        instrument   
+    end
+
+  end
 end
